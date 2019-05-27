452e3961700fe1994fe6880edf68a6f65744d748
Zstar <1596818794@qq.com>
2019-05-27 15:59:51 +0800

    commit info

---
+++ src/leetcode/Main.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+public class Main {
+	
+	private static void print(Object o) {
+		System.out.println(o.toString());
+		
+	}
+	
+
+	public static void main(String[] args) {
+		print("Hello world!");
+
+	}
+
+	
+
+}
